6ca1abc732dc3745d6e726cd3fe5f89507890d0f
Md Liton Mahmud <litonmahmud0023@gmail.com>
2024-02-16 03:26:27 +0600

    bug

--- api/index.js
+++ api/index.js
@@ -41,15 +41,17 @@ app.get("/", (req, res) => {
     console.log(` Boss is running on ${port}`);
   });
 
-  app.use(express.static(path.join(_dirname, '/client/dist')));
-
-app.get('*', (req, res) => {
-  res.sendFile(path.join(_dirname, 'client', 'dist', 'index.html'));
-})
 
   app.use('/api/user', userRouter);
   app.use('/api/auth', authRouter);
-  app.use('/api/listing',listingRouter)
+  app.use('/api/listing',listingRouter);
+
+
+  app.use(express.static(path.join(_dirname, '/client/dist')));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.join(_dirname, 'client', 'dist', 'index.html'));
+  })
 
 
   app.use((err, req, res, next) => {
