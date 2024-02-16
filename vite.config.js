187939b169db8f453be2e62e8a2e3a011e1da218
Md Liton Mahmud <litonmahmud0023@gmail.com>
2024-02-17 00:39:55 +0600

    remove firebase

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'https://server-side-olive.vercel.app',
+        target: `http://localhost:3000`,
         secure: false,
       },
     },
