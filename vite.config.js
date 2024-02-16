bdab7f4f0a4c3e9ee12a26ec0f3445472d3adb15
Md Liton Mahmud <litonmahmud0023@gmail.com>
2024-02-16 06:26:51 +0600

    add vercel api

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:3000',
+        target: 'https://server-side-olive.vercel.app',
         secure: false,
       },
     },
