2aa5392179af98dbe05a3bd7da1f0724584763f6
Md Liton Mahmud <litonmahmud0023@gmail.com>
2024-02-17 05:20:18 +0600

    serverlink

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: `http://localhost:3000`,
+        target: `https://mern-server-9cwo.onrender.com`,
         secure: false,
       },
     },
