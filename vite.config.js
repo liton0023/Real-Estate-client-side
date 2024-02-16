b35495638e1b3d70f1b3faf2f210879fb8436bf4
Md Liton Mahmud <litonmahmud0023@gmail.com>
2024-02-17 00:59:04 +0600

    sirver side link add

--- vite.config.js
+++ vite.config.js
@@ -6,7 +6,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: `http://localhost:3000`,
+        target: `http://server-side-m9du.onrender.com`,
         secure: false,
       },
     },
