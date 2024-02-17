0b19db8b27adad7f3a030ef3b39d98da8d69c9a5
Md Liton Mahmud <litonmahmud0023@gmail.com>
2024-02-18 01:01:20 +0600

    add url

--- vite.config.js
+++ vite.config.js
@@ -3,16 +3,5 @@ import { defineConfig } from 'vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  server: {
-    port:5000,
-    proxy: {
-        '/api': {
-        target: "http://mern-server-9cwo.onrender.com",
-        changeOrigin:true,
-        rewrite: (path)=> path.replace(/^\/api/,"/api"),
-        // secure: true,
-      },
-    },
-  },
   plugins: [react()],
 })
